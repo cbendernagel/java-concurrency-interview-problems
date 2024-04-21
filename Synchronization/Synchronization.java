35b15e0631e1f3c6aecd7c68cc892d1d1c896d12
cbenderngel <cbendernagel@gmail.com>
2024-04-21 18:25:29 -0400

    synchronized lrt with interrupt

---
+++ Synchronization/Synchronization.java
@@ -0,0 +1,18 @@
+package Synchronization;
+
+import java.util.concurrent.TimeUnit;
+
+public class Synchronization {
+    
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("hello world!");
+        LongRunningThread thread = new LongRunningThread();
+
+        thread.start();
+
+        TimeUnit.MILLISECONDS.sleep(10000);
+
+        thread.cancel();
+    }
+}
